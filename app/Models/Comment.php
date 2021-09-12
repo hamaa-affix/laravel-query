e49cc53a3c48b67b8aff9f9fb4ae747ff018d07b
hamaa-affix <hamaagive1108@gmail.com>
2021-09-12 22:37:32 +0900

    ミューテーターの実装

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -9,11 +9,21 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Comment extends Model
 {
     use SoftDeletes;
-    
+
     protected $guarded = [];
 
     public function User(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
+
+    /**
+     * アクセッサー
+     * 使いたい時は$this->who_what;とで値をリターンしてくれる。
+     */
+
+     public function getWhoWhatAttribute()
+     {
+         return "user {$this->user_id} rating {$this->rating}";
+     }
 }
