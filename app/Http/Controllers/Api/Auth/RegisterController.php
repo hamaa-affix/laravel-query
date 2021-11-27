44cf8dbf83ed31460a5e061716f1a6e1ef94001a
hamaa-affix <hamaagive1108@gmail.com>
2021-11-27 21:38:20 +0900

    user登録api修正

--- app/Http/Controllers/Api/Auth/RegisterController.php
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Company;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
+use PhpParser\Node\Stmt\TryCatch;
 
 class RegisterController extends Controller
 {
@@ -56,6 +61,7 @@ class RegisterController extends Controller
             'lastName' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'companyName' => ['required', 'string', 'max:255']
         ]);
     }
 
@@ -63,28 +69,66 @@ class RegisterController extends Controller
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
+     * @param Company $company
      * @return \App\User
      */
-    protected function create(array $data)
+    protected function create(array $data, Company $company)
     {
         return User::create([
             'first_name' => $data['firstName'],
             'last_name' => $data['lastName'],
+            'company_id' => $company->id,
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
     }
 
-    public function register(Request $request): JsonResponse
+    /**
+     * 企業を検索して、見つからなければ、作成し返却します。
+     * @param int $campanyId
+     * @param array $companyData
+     * @return Company
+     */
+    protected function registerCampany(int $companyId = null, array $companyData): Company
+    {
+        Log::debug("会社登録開始", ['compant_data' => $companyData]);
+        $company = Company::find($companyId);
+
+        if(empty($company)) {
+            return Company::create($companyData);
+        }
+
+        return $company;
+    }
+
+    public function register(Request $request)
     {
         $validate = $this->validator($request->all());
 
+        //企業登録も行っちゃう。企業情報がなければ、後で保存する？？
         if ($validate->fails()) {
             return new JsonResponse($validate->errors());
         }
+        try {
+            DB::transaction(function () use ($request) {
+                $company = [
+                    'name' => $request->companyName
+                ];
+                Log::debug("comapanyId", ['id' => $request->companyId]);
+                $company = $this->registerCampany($request->companyId, $company);
+                event(new Registered($user = $this->create($request->all(), $company)));
 
-        event(new Registered($user = $this->create($request->all())));
+                Log::debug("user作成！", ['user' => $user]);
+                return response()->json([
+                    'message' => '登録しました'
+                ], 200);
+            });
+        } catch(\Throwable $e) {
+            Log::error($e->getMessage());
+            return response()->json([
+                'message' => '登録に失敗しました'
+            ], 500);
+        }
 
-        return new JsonResponse($user);
     }
 }
