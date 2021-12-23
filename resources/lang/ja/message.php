34e2e20f5fbc1e6713309a6f63609a8a1cc5e85b
hamaa-affix <hamaagive1108@gmail.com>
2021-12-23 23:59:59 +0900

    メッセージの作成と日本語化

---
+++ resources/lang/ja/message.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'success' => [
+        'user' => [
+            'update' => 'profile更新しました',
+        ]
+    ],
+
+    'error' => [
+        'user' => [
+            'update' => 'profile更新に失敗しました',
+        ]
+    ]
+];
