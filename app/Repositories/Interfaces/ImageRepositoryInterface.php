753b777101485be7c50d61c1c28286105ef0df23
hamaa-affix <hamaagive1108@gmail.com>
2022-01-10 16:51:58 +0900

    valueObject, Entity仮作成

---
+++ app/Repositories/Interfaces/ImageRepositoryInterface.php
@@ -0,0 +1,25 @@
+<?php 
+namespace App\Repositories\Interfaces;
+use Illuminate\Support\Collection;
+use App\Models\User;
+use packages\Domain\Entities\Image\ImageModel;
+use packages\Domain\ValueObjects\Image\Image;
+use packages\Domain\ValueObjects\User\UserId;
+
+interface ImageRepositoryInterface
+{
+
+    /**
+     * 特定のuserと紐ずく画像一覧を取得します。
+     * @param UserId $id
+     * @return ImageModel
+     */
+    public function fetchImage(UserId $id): ImageModel
+
+    /**
+     * 画像pathを保存します。
+     * @param Image $image
+     * @return void
+     */
+    public function store(Image $iamge): void
+}
